53f074427509a2ad887f9e0f73f84e6e7f506cf6
timmyyeh <timmyyeh@cs.washington.edu>
2019-01-08 22:17:36 -0800

    refactor blog route

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
+// router
+const blogRouter = require('./routes/blog.js')
+
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/blog/new', (req, res) => {
-    res.send("new");
-});
+app.use(blogRouter)
+
 
 app.listen(8080, 'localhost', function() {
     console.log("Server is now running..");
