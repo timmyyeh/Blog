5993a61694d12bfadbd067ee3bfc36c7c0cc01a7
timmyyeh <timmyyeh@cs.washington.edu>
2019-01-13 01:22:19 -0800

    add route for user.js

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,20 @@
 const app = require('express')
 const router = app.Router();
 
+router.get('/user/login', (req, res) => {
+    res.send('login');
+});
 
+router.get('/user/logout', (req, res) => {
+    res.send('logout');
+});
+
+router.get('/user/new', (req, res) => {
+    res.send('redirect to form');
+})
+
+router.post('/user', (req, res) => {
+    res.send('new user');
+})
 
 module.exports = router;
